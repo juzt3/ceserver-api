ea993de4086743d00aed8049fe8ad6e35ae7439f
Felipe Gatica (Paragati Das) <juzt3@users.noreply.github.com>
2025-05-30 00:54:33 -0400

    more methods added
    is android and every type of data was added

--- client.py
+++ client.py
@@ -45,6 +45,11 @@ class CEServerClient:
         version_string = self._sock.recv(ce_version.stringsize)
         self.log.info(f"Server version: {version_string.decode()}")
 
+    def is_android(self) -> bool:
+        self._send_command(CE_CMD.CMD_ISANDROID)
+        data = self._sock.recv(1)
+        return bool(data[0])
+
     def disconnect(self):
         self._send_command(CE_CMD.CMD_CLOSECONNECTION)
         self._sock.close()
@@ -116,14 +121,83 @@ class CEServerClient:
         value = self._sock.recv(response_size)
         return value
 
-    def read_int32(self, address: int, compress: int = 0) -> int | None:
+    def read_byte(self, address: int, compress: int = 0) -> int | None:
+        data = self.read_process_memory(address, 1, compress)
+        if data is None:
+            return None
+        return struct.unpack('<B', data)[0]
+
+    def read_int16(self, address: int, compress: int = 0, signed: bool = True) -> int | None:
+        data = self.read_process_memory(address, 2, compress)
+        if data is None:
+            return None
+        fmt = "<h" if signed else "<H"
+        return struct.unpack(fmt, data)[0]
+
+    def read_uint16(self, address: int, compress: int = 0) -> int | None:
+        return self.read_int16(address, compress, False)
+
+    def read_int32(self, address: int, compress: int = 0, signed: bool = True) -> int | None:
         value = self.read_process_memory(address, 4, compress)
         if value is None:
             return None
-        return struct.unpack("<L", value)[0]
+        fmt = "<l" if signed else "<L"
+        return struct.unpack(fmt, value)[0]
+
+    def read_uint32(self, address: int, compress: int = 0) -> int | None:
+        return self.read_int32(address, compress, False)
+
+    def read_int64(self, address: int, compress: int = 0, signed: bool = True) -> int | None:
+        value = self.read_process_memory(address, 8, compress)
+        if value is None:
+            return None
+        fmt = "<q" if signed else "<Q"
+        return struct.unpack(fmt, value)[0]
+
+    def read_uint64(self, address: int, compress: int = 0) -> int | None:
+        return self.read_int64(address, compress, False)
 
     def read_float(self, address: int, compress: int = 0) -> float | None:
         value = self.read_process_memory(address, 4, compress)
         if value is None:
             return None
         return struct.unpack("<f", value)[0]
+
+    def read_double(self, address: int, compress: int = 0) -> float | None:
+        value = self.read_process_memory(address, 8, compress)
+        if value is None:
+            return None
+        return struct.unpack("<d", value)[0]
+
+    def read_bytes(self, address: int, length: int, compress: int = 0):
+        return self.read_process_memory(address, length, compress)
+
+    def read_str(self, address: int, length: int = 256, unicode=False, compress: int = 0) -> str | None:
+        data = self.read_bytes(address, length, compress)
+        if data is None:
+            return None
+
+        try:
+            string_bytes = data.split(b'\x00', 1)[0]  # cortar en nulo si existe
+            encoding = "utf-16" if unicode else "utf-8"
+            return string_bytes.decode(encoding, errors='ignore')
+        except Exception as e:
+            self.log.error(f"[!] Error decoding string: {e}")
+            return None
+
+    def read_ptr(self, address: int, compress: int = 0) -> int | None:
+        ptr = self.read_uint64(address, compress)
+        MIN_ADDR = 0x10000
+        # Likely an invalid pointer
+        if ptr is None or ptr < MIN_ADDR:
+            return None
+        return ptr
+
+    def read_pointer_chain(self, base: int, offsets: list[int], compress: int = 0) -> int | None:
+        addr = base
+        for off in offsets:
+            ptr = self.read_ptr(addr + off, compress)
+            if ptr is None:
+                return None
+            addr = ptr
+        return addr
